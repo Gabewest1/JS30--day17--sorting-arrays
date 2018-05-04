9c607fee4315a4c0c057ecf4a6485e9e20229776
Gabriel West <gabe_west1@yahoo.com>
2018-05-03 22:32:16 -0500

    Created sort function to alphabetically sort the bands array, not including bands who names start with: 'a, an or the'

---
+++ index.js
@@ -0,0 +1,30 @@
+const bands = [
+    'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean',
+    'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts',
+    'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+]
+
+function sort(arr) {
+    let arrWithoutStartingWords = arr.map(word => {
+        let lowercased = word.toLowerCase()
+        if (lowercased.split(" ")[0] === "an") {
+            return lowercased.replace("an", "").trim()
+        } else if (lowercased.split(" ")[0] === "a") {
+            return lowercased.replace("a", "").trim()
+        } else if (lowercased.split(" ")[0] === "the") {
+            return lowercased.replace("the", "").trim()
+        }
+
+        return lowercased
+    })
+
+    let remember = arrWithoutStartingWords.reduce((obj, current, i) => {
+        obj[current] = arr[i]
+        return obj
+    }, {})
+
+    let sortedArr = arrWithoutStartingWords.sort((a, b) => a > b ? 1 : -1)
+    return sortedArr.map(val => remember[val])
+}
+
+console.log(sort(bands))
